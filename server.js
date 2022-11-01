715a0b945d1eee42f4e75eb089d3d3655837a290
Ervin Castelino <ervincas13@gmail.com>
2022-11-01 12:23:32 +0530

    Initial Author Routes

--- server.js
+++ server.js
@@ -7,14 +7,18 @@ const express = require('express')
 const expressLayouts= require('express-ejs-layouts')
 const app = express();
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 const mongoose = require('mongoose');
+const bodyParser= require('body-parser')
 
+app.use(bodyParser.urlencoded({limit:'10mb', extended:false}))
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts)
 app.use(express.static('public'));
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true
